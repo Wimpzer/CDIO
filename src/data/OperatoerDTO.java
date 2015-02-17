4d52c1c0bae8690b08e16dafa368ca760f610459
alstroem <alstroem1992@gmail.com>
2015-02-17 14:34:11 +0100

    Skidt

---
+++ src/data/OperatoerDTO.java
@@ -0,0 +1,43 @@
+package data;
+
+public class OperatoerDTO {
+
+	int oprId;                     
+	String oprNavn;                
+	String ini;                 
+	String cpr;                 
+	String password;
+
+
+	public int getOprId() {
+		return oprId;
+	}
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
+	}
+	public String getOprNavn() {
+		return oprNavn;
+	}
+	public void setOprNavn(String oprNavn) {
+		this.oprNavn = oprNavn;
+	}
+	public String getIni() {
+		return ini;
+	}
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+	public String getCpr() {
+		return cpr;
+	}
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
